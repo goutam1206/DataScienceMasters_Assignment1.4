1a716ae74967dcf7a0af5a60f1a141bd6474bcc6
Goutama Sarma Mylavarapu <sarma.goutama@gmail.com>
2018-01-14 12:28:53 +0530

    Initial Commit
    Program to calculate the Volume of a sphere with diameter 12.
    Output also is available as a screenshot

---
+++ Assignment1.4.py
@@ -0,0 +1,6 @@
+import math
+#Radius of the Sphere to be calculated from diameter
+d = 12
+r = d/2
+var = (4/3) * math.pi * ( r  ** 3)
+print ("Volume of sphere with diameter " + str(d) + " is "+ str(var))
